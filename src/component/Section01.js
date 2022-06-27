01d2db978010bda076e1a60e9bcba7e008e922d2
Jyngxv <jyng@pood.pet>
2022-06-27 12:59:57 +0900

    Initial Commit

---
+++ src/component/Section01.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Section01 = () => {
+    return (
+        <div>
+            <button className='bg-red-500 pt-2'>button</button>
+        </div>
+    );
+};
+
+export default Section01;
